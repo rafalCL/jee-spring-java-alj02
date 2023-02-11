616967ac7427faaa5aac8773dd1a384efecdbac0
edu <edu@mail.pl>
2023-02-11 14:56:32 +0100

    m5d1 resolve NoUniqueBeanDefinitionException

--- src/main/java/pl/coderslab/app/beans/MessageSender.java
+++ src/main/java/pl/coderslab/app/beans/MessageSender.java
@@ -1,12 +1,13 @@
 package pl.coderslab.app.beans;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MessageSender {
     private MessageService service;
 
-    public MessageSender(MessageService service) {
+    public MessageSender(@Qualifier("smsService") MessageService service) {
         this.service = service;
     }
 
--- src/main/java/pl/coderslab/app/beans/SmsService.java
+++ src/main/java/pl/coderslab/app/beans/SmsService.java
@@ -4,10 +4,9 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary
-public class EmailService implements MessageService {
+public class SmsService implements MessageService {
     @Override
     public void send(String msg) {
-        System.out.println("sending email with body: " + msg);
+        System.out.println("sending SMS with body: " + msg);
     }
 }
